0395c276361ef98ae69de90540783bd0d5745b06
2200704780 <145361541+2200704780@users.noreply.github.com>
2024-02-05 21:14:45 +0300

    second

---
+++ script.js
@@ -0,0 +1,25 @@
+//console.log("hello BBC3");
+
+//console.log(2);
+// console.log(8+2);
+//  var name= "irene";
+//  let age= 17;
+  
+//  const interestRate = 0.23;
+// let isActive = true;
+// let universityName= null;
+// let marks;
+//  console.log(isActive);
+//  console.log(universityName);
+//  console.log(marks);
+ 
+// let name= 'Ssembeguya_ivan';
+// console.log(name[name.length -5]);
+// let name= 'SsembeguyaIvan';
+// console.log([name.length]);
+//  let number= 45 *23;
+//  console.log(number);
+
+
+let marks = null;
+console.log(marks);
